498eed72952875d62d586fc065deb4f59d6cbcb4
MondeMbono <monde@github.com>
2023-02-21 10:50:02 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets in lowercase
+ * Return: 0
+ * This program was written by Monde Mbono
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+
+}
