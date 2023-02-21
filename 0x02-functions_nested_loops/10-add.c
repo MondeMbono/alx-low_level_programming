58e3674ddccff29baa6a759f3a29d94dd06576a2
MondeMbono <monde@github.com>
2023-02-21 11:47:29 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function taht'll add
+ * @f: first int
+ * @s: second int
+ * Return: sum of f and s
+ * By Monde
+ */
+int add(int f, int s)
+{
+
+	return (f + s);
+
+}
