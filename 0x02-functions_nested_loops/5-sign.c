888636d52cd10e654a732249e4474364c4885975
MondeMbono <monde@github.com>
2023-02-21 11:34:03 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - function thatprints a sign of a number
+ * @n: will represent the number being printed
+ * Return: 1 if n is positive, 0 if n is 0, and -1 if n is less than zero
+ * By Monde
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
