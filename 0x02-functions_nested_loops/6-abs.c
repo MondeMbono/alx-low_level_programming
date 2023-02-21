817d16e1df94c29b35593ec99e98d1b9860b7e9f
MondeMbono <monde@github.com>
2023-02-21 11:36:52 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value
+ * @z: will return absolut value
+ * Return: returns absolute value
+ * By Monde
+ */
+int _abs(int z)
+{
+
+
+	if (z < 0)
+	{
+		return (-z);
+	}
+	else
+	{
+		return (z);
+	}
+
+}
