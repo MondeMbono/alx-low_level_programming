e4e0a3b8b24201815948547919c0abc0191b1f88
MondeMbono <monde@github.com>
2023-02-21 11:39:44 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_last_digit - will print the last digit of a number
+ * @p: value that will be printed
+ * Return: last digit
+ * By Monde
+ */
+int print_last_digit(int p)
+{
+	int v = p % 10;
+
+	if (v < 0)
+	{
+		v *= -1;
+		_putchar(v + '0');
+	}
+
+
+	else
+	{
+		_putchar(v + '0');
+	}
+
+
+	return (v);
+
+}
