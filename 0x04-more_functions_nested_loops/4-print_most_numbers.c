0623df99d1df1776d789631bde882071398fde85
MondeMbono <monde@github.com>
2023-02-23 07:51:52 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,22 +3,16 @@
  * print_most_numbers - function that will print from 0 -9 and skip 2 and 4
  * Return: void
  */
+
 void print_most_numbers(void)
 {
-	int p;
+	char c;
 
-	for (p = 48; p < 58; p++)
+	for (c = '0'; c <= '9'; c++)
 	{
-
-		if ((p != 50) && (p != 52))
-		{
-			_putchar(p);
-		}
-		else
-		{
-			continue;
-		}
-		_putchar('\n');
-
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
+	_putchar('\n');
 }
+
